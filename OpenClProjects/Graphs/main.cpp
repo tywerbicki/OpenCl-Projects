d03d2977ad940d5b156325a468f8c422bf370b96
tywerbicki <ty.werbicki@gmail.com>
2023-08-03 22:25:05 -0700

    Changed spacing.

--- OpenClProjects/Graphs/main.cpp
+++ OpenClProjects/Graphs/main.cpp
@@ -8,87 +8,87 @@
 
 cl_uint DisplayPlatformInfo(const cl_platform_id platformId)
 {
-	std::array<cl_platform_info, 5> paramNames{
-		CL_PLATFORM_PROFILE,
-		CL_PLATFORM_VERSION,
-		CL_PLATFORM_NAME,
-		CL_PLATFORM_VENDOR,
-		CL_PLATFORM_EXTENSIONS
-	};
+    std::array<cl_platform_info, 5> paramNames{
+        CL_PLATFORM_PROFILE,
+        CL_PLATFORM_VERSION,
+        CL_PLATFORM_NAME,
+        CL_PLATFORM_VENDOR,
+        CL_PLATFORM_EXTENSIONS
+    };
 
-	std::array<const char*, 5> paramNameStrs{
-		"CL_PLATFORM_PROFILE",
-		"CL_PLATFORM_VERSION",
-		"CL_PLATFORM_NAME",
-		"CL_PLATFORM_VENDOR",
-		"CL_PLATFORM_EXTENSIONS"
-	};
+    std::array<const char*, 5> paramNameStrs{
+        "CL_PLATFORM_PROFILE",
+        "CL_PLATFORM_VERSION",
+        "CL_PLATFORM_NAME",
+        "CL_PLATFORM_VENDOR",
+        "CL_PLATFORM_EXTENSIONS"
+    };
 
-	for (size_t i = 0; i < paramNames.size(); i++)
-	{
-		cl_uint result                = CL_SUCCESS;
-		size_t  paramValueSizeInBytes = 0;
+    for (size_t i = 0; i < paramNames.size(); i++)
+    {
+        cl_uint result                = CL_SUCCESS;
+        size_t  paramValueSizeInBytes = 0;
 
-		// Query size of parameter value.
-		result = clGetPlatformInfo(
-			platformId,
-			paramNames[i],
-			0,
-			nullptr,
-			&paramValueSizeInBytes
-		);
+        // Query size of parameter value.
+        result = clGetPlatformInfo(
+            platformId,
+            paramNames[i],
+            0,
+            nullptr,
+            &paramValueSizeInBytes
+        );
 
-		if (result != CL_SUCCESS)
-		{
-			return result;
-		}
+        if (result != CL_SUCCESS)
+        {
+            return result;
+        }
 
-		std::unique_ptr<char[]> paramValue{ new char[paramValueSizeInBytes] };
+        std::unique_ptr<char[]> paramValue{ new char[paramValueSizeInBytes] };
 
-		// Query parameter value.
-		result = clGetPlatformInfo(
-			platformId,
-			paramNames[i],
-			paramValueSizeInBytes,
-			paramValue.get(),
-			nullptr
-		);
+        // Query parameter value.
+        result = clGetPlatformInfo(
+            platformId,
+            paramNames[i],
+            paramValueSizeInBytes,
+            paramValue.get(),
+            nullptr
+        );
 
-		if (result != CL_SUCCESS)
-		{
-			return result;
-		}
+        if (result != CL_SUCCESS)
+        {
+            return result;
+        }
 
-		std::cout << paramNameStrs[i] << ": " << paramValue << "\n";
-	}
+        std::cout << paramNameStrs[i] << ": " << paramValue << "\n";
+    }
 }
 
 
 int main()
 {
-	cl_uint result     = CL_SUCCESS;
-	cl_uint nPlatforms = 0;
+    cl_uint result     = CL_SUCCESS;
+    cl_uint nPlatforms = 0;
 
-	// Query the number of available platforms.
-	result = clGetPlatformIDs(0, nullptr, &nPlatforms);
+    // Query the number of available platforms.
+    result = clGetPlatformIDs(0, nullptr, &nPlatforms);
 
-	if (result != CL_SUCCESS)
-	{
-		return result;
-	}
+    if (result != CL_SUCCESS)
+    {
+        return result;
+    }
 
-	std::vector<cl_platform_id> platformIds{nPlatforms};
+    std::vector<cl_platform_id> platformIds{nPlatforms};
 
-	// Query the available platform ids.
-	result = clGetPlatformIDs(nPlatforms, platformIds.data(), nullptr);
+    // Query the available platform ids.
+    result = clGetPlatformIDs(nPlatforms, platformIds.data(), nullptr);
 
-	if (result != CL_SUCCESS)
-	{
-		return result;
-	}
+    if (result != CL_SUCCESS)
+    {
+        return result;
+    }
 
-	for (size_t i = 0; i < nPlatforms; i++)
-	{
-		DisplayPlatformInfo(platformIds[i]);
-	}
+    for (size_t i = 0; i < nPlatforms; i++)
+    {
+        DisplayPlatformInfo(platformIds[i]);
+    }
 }
